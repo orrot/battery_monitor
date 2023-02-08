31f073bbba05dfb3425dd6d754205e225bf28394
Orlando <sort465@gmail.com>
2023-02-08 07:55:12 -0500

    Adding delay at start

---
+++ battery_restart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/orrot/Programs/path/battery_stop.sh;
+(nohup /home/orrot/Programs/path/battery_monitor.sh &);
--- battery_start.sh
+++ battery_start.sh
@@ -3,5 +3,11 @@
 # Use Command Output widget
 # install acpi
 
+truncate -s 0 ~/Battery/batstat
+echo ' --- ' >> ~/Battery/batstat
+
+sleep 240;
+
+
 (nohup /home/orrot/Programs/path/battery_monitor.sh) &
 exit 0;
